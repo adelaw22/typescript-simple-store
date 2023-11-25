159749d9770180302648483329a4cc14a3aebbd8
Adelaw22 <Adelegbelawrence@gmail.com>
2023-11-25 01:48:30 +0100

    create dynamic react portal component

---
+++ src/components/ReactPortal.tsx
@@ -0,0 +1,44 @@
+import {ReactNode, useState, useLayoutEffect} from "react"
+import {createPortal} from 'react-dom'
+
+const defaultReactPortalProps = {
+    wrapperId: 'react-portal'
+}
+
+type ReactPortalProps = {
+    children: ReactNode,
+    wrapperId: string,
+} & typeof defaultReactPortalProps;
+
+const ReactPortal = ({children, wrapperId}: ReactPortalProps)=>{
+    const [wrapper, setWrapper]= useState <Element | null>(null)
+
+    useLayoutEffect(() => {
+      let element = document.getElementById(wrapperId);
+      let created = false
+       
+      if (!element){
+        created =true;
+        const wrapper = document.createElement('div')
+        wrapper.setAttribute('id', wrapperId)
+        document.body.appendChild(wrapper)
+        element = wrapper
+      }
+
+      setWrapper(element)
+    
+      return () => {
+            if (created && element?.parentNode){
+                element.parentNode.removeChild(element)
+            }
+      };
+    }, [wrapperId])
+
+   if (wrapper === null) return null;
+
+   return createPortal(children, wrapper);
+}
+
+ReactPortal.defaultProps = defaultReactPortalProps;
+
+export default ReactPortal
