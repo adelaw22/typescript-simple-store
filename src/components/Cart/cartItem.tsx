ae2d56e835013dac099aaa9b0e6e452b55302e12
Adelaw22 <Adelegbelawrence@gmail.com>
2024-05-16 01:40:53 +0100

    fix: revamp cart ui

--- src/components/Cart/cartItem.tsx
+++ src/components/Cart/cartItem.tsx
@@ -1,23 +1,26 @@
 import { TrashIcon } from '@heroicons/react/24/outline'
+import BackPack from "../../assets/images/backpack.png"
 
 const cartItem = () => {
   return (
-    <div>
-        <div>
-            <img src='' alt=""/>
-        </div>
-
-        <div>
-            <p>Name Of Product</p>
+    <div className='flex justify-between'>
+       <div className=' flex '>
+            <div className='bg-[#EBECF2] w-[9rem] p-3 rounded-md'>
+                <img src={BackPack} alt="" className='w-7/5 object-contain'/>
+            </div>
 
             <div>
-                <button>-</button>
-                <span>1</span>
-                <button>+</button>
-            </div>
-        </div>
+                <p>Name Of Product</p>
 
-        <div>
+                <div className=''>
+                    <button>-</button>
+                    <span>1</span>
+                    <button>+</button>
+                </div>
+            </div>
+
+       </div>
+        <div className='align-right'>
             <TrashIcon className='h-5 w-5 stroke-gray-700 stroke-2 cursor-pointer'/>
             <p>$200.00</p>
         </div>
--- src/components/Cart/cartModel.tsx
+++ src/components/Cart/cartModel.tsx
@@ -3,12 +3,13 @@ import ReactPortal from '../ReactPortal'
 import CustomBtn from '../customBtn';
 import CartItem from './cartItem';
 import { useNavbar } from '../../hooks/useNavbar';
+import { XMarkIcon } from '@heroicons/react/24/outline';
 
 // interface CartModalProps{
 //   title?: string;
 // }
 const componentStyles ={
-  hrStyle: "border-t-1 border-t-neutral-400",
+  hrStyle: "border-t-1 border-t-[#DADBE6] ",
 }
 
 interface CartModalProps{
@@ -21,40 +22,39 @@ const CartModal=({ isOpen, setIsOpen }:CartModalProps)=> {
   
   const closeModal = ()=>{
     setIsOpen(false)
-}
+  }
 
   return (
    <>
     {isOpen &&
         <> 
-        <div  className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="bg-white">
-            <div className='flex justify-between'>
-                <div>
-                  <p className="text-xl font-bold">Shopping cart</p>
-                  <p>0 item</p>
+          <div  className="fixed inset-0 flex  justify-end z-50">
+            <div className="bg-white h-screen w-2/5 px-5">
+              <div className='flex justify-between'>
+                  <div>
+                    <p className="text-xl font-bold">Shopping cart</p>
+                  </div>
+
+                  <div onClick={closeModal}>
+                    <XMarkIcon className="h-6 w-6 text-gray-500 cursor-pointer" />
+                  </div>
                 </div>
 
-                <div onClick={closeModal}></div>
-              </div>
+                <hr className={componentStyles.hrStyle}/>
+                  {/* Cart items */}
+                  <CartItem/>
 
-              <hr className={componentStyles.hrStyle}/>
-                {/* Cart items */}
-                <CartItem/>
-
-                <div className='position-sticky bottom-0'>
-                    <div className='flex justify-between'>
-                      <p>Total:</p>
-                      <p></p>
-                    </div>
-                    <CustomBtn>Checkout</CustomBtn>
-                </div>
-
-                
+                  <div className='position-sticky bottom-0'>
+                      <div className='flex justify-between'>
+                        <p>Total:</p>
+                        <p></p>
+                      </div>
+                      <CustomBtn>Checkout</CustomBtn>
+                  </div>
+            </div>
           </div>
-        </div>
-      </>
-   }
+        </>
+      }
    </>
   )
 }
